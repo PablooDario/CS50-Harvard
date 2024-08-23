e21aad82cbbaba3a344225033dc9c8186defd702
Pablo Sánchez <pabloosancheez0@gmail.com>
2024-08-22 23:22:03 -0600

    View wirh Aggregation Functions

---
+++ CS50-SQL/Week 4 Viewing/census/by_district.sql
@@ -0,0 +1,6 @@
+-- Create a view that contains the sums for each numeric column in census, across all districts and localities
+
+CREATE VIEW "by_district" AS
+SELECT district, SUM(families) AS "families", SUM(households) AS "households", SUM(population) AS "population", SUM(male) AS "male", SUM(female) AS "female"
+FROM census
+GROUP BY district;
---
+++ CS50-SQL/Week 4 Viewing/census/total.sql
@@ -0,0 +1,5 @@
+-- Create a view that contains the sums for each numeric column in census, across all districts and localities
+
+CREATE VIEW "total" AS
+SELECT SUM(families) AS "families", SUM(households) AS "households", SUM(population) AS "population", SUM(male) AS "male", SUM(female) AS "female"
+FROM census;
